c27ae008254625658f49bd498d13edfcf5151409
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-11-30 21:22:50 -0300

    Otimização de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <div class="app">
+      <app-header id="content"></app-header>
+      <div class="container">
+        <router-outlet></router-outlet>
+      </div>
+      <app-footer></app-footer>
+    </div>
+  `,
+  styles: [
+    `
+      .app {
+        background: #e0efef;
+        color: #0b1641;
+        min-height: 100%;
+      }
+      @media (max-width: 530px) {
+        #content {
+          background: #e0efef;
+          color: #0b1641;
+        }
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'curriculum';
